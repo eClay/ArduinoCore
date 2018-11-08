fc86d439e22e4c0a1a2051d1a548ed53963acb4a
Greg Mrozek <greg.mrozek@eclay.com>
2018-11-08 08:06:38 -0600

    moved arduino api folder directly into repository rather than soft link

---
+++ cores/samd21a/src/arduino/ArduinoAPI.h
@@ -0,0 +1,30 @@
+#ifndef ARDUINO_API_H_
+#define ARDUINO_API_H_
+
+#include "A_Analog.h"
+#include "A_Character.h"
+#include "A_Client.h"
+#include "A_Delay.h"
+#include "A_Digital.h"
+#include "A_HardwareI2C.h"
+#include "A_HardwareSerial.h"
+#include "A_Interrupts.h"
+#include "A_Math.h"å
+#include "A_Memory.h"
+#include "A_PluggableUSB.h"
+#include "A_Print.h"
+#include "A_Printable.h"
+#include "A_Pulse.h"
+#include "A_Random.h"
+#include "A_RingBuffer.h"
+#include "A_Server.h"
+#include "A_Shift.h"
+#include "A_Stream.h"
+#include "A_String.h"
+#include "A_Time.h"
+#include "A_Tone.h"
+#include "A_Types.h"
+#include "A_Udp.h"
+#include "A_USBAPI.h"
+
+#endif  // ARDUINO_API_H_
